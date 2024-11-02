4e41dd2207fdde42c4547f814341bed37215fff7
jessicasanri <js.ribeiro91@gmail.com>
2024-11-01 22:03:02 -0300

    init

---
+++ assets/js/scripts.js
@@ -0,0 +1,14 @@
+const menu = document.querySelector("#btn-menu")
+
+menu.addEventListener("click", () => {
+
+    console.log("Clicou")
+
+})
+
+const botaoEstouComSorte = document.querySelector('form [type="button"]')
+
+botaoEstouComSorte.addEventListener("click", () => {
+
+    location.href = "https://google.com"
+})
